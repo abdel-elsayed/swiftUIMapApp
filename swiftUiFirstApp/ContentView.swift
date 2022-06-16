6de3215f20a7707f3a1e61fddee85fdbba243a66
abdel-elsayed <abdel.elsayed18@gmail.com>
2022-06-16 17:29:39 -0400

    Initial Commit

---
+++ swiftUiFirstApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUiFirstApp
+//
+//  Created by Abdelrahman Elsayed on 6/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftUiFirstApp/swiftUiFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUiFirstAppApp.swift
+//  swiftUiFirstApp
+//
+//  Created by Abdelrahman Elsayed on 6/16/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftUiFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
